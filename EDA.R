8ad18af38991dfc3f3a3fa79c678c4b4f071a764
sebasAVA <js.rojas551@gmail.com>
2022-01-05 18:11:32 -0500

    ARIMA

--- EDA.R
+++ EDA.R
@@ -15,12 +15,19 @@ library("lubridate")
 require(xts)
 library(scales)
 library(fpp2)
+library(seasonal)
 #Establecer un directorio de trabajo
 setwd("C:/Users/sebas/Documents/2021-20/BUSINESS ECONOMIC AND FINANCIAL DATA/BEFD Scripts/Data Merge")
 
 dfEE <- read.csv("EE2.csv")
 
-#Graficas
+###########################################################################
+###########################################################################
+###                                                                     ###
+###                        PREPROCESSING OF DATA                        ###
+###                                                                     ###
+###########################################################################
+###########################################################################
 
 
 dfEE$date <- as.Date(dfEE$Fecha)
@@ -75,22 +82,42 @@ dfEE$DateTime = as.POSIXct(strptime(dfEE$DateTime, format ="%Y-%m-%d %H:%M"))
 firstHour <- 24*(as.Date("2003-1-1 00:00:00")-as.Date("2003-1-1 00:00:00"))
 tsEE <- ts(dfEE,start=c(2003,firstHour),frequency=24*365)
 tsShort <- window(tsEE, 2015,2016)
+tsDecomposition <- window(tsEE, 2015,2018)
+
+
+tsShortAlt <- window(tsEE, 2018,2019)
 #sdts = xts(dfEE, order.by = dfEE$DateTime)
 # Subset training
-Seasonal =  subset(dfEE, DateTime> "2020-12-1 00:00:00" & date < "2020-12-30 00:00:00")
+Seasonal =  subset(dfEE, DateTime> "2020-12-1 00:00:00" & date < "2020-12-31 12:00:00")
+
+tsSeasonal <- ts(Seasonal$Demanda,start=c(2020,firstHour),frequency=24)
+
 
 LongTime =  subset(dfEE, DateTime> "2019-1-1 00:00:00" & date < "2020-12-30 00:00:00")
-#------------------------------------------------------------------------------------------------------------------------------#
-#-------------------------------------------------------------------------------------------------------------------------------# 
-#dfEE <- dfEE[105181:113940,]
 
+############################################################################
+############################################################################
+###                                                                      ###
+###                         CREACION DE GRAFICAS                         ###
+###                                                                      ###
+############################################################################
+############################################################################
 
+#Faltan Histogramas de cositas
 
 ggplot(Seasonal, aes(DateTime))+
   geom_line(aes(y = Demanda, colour = "Demand"),size=1.2)+
   geom_line(aes(y = Oferta ,colour = "Offer"),size=0.8)+
   scale_y_continuous(  labels = scales::number_format( big.mark = ',')) +
   labs(title = "Offer and Demand of energy hourly[kWh]", y = "Energy [kWh]")
+
+tsEE %>%
+  as.data.frame() %>%
+  ggplot(aes(x=DateTime, y=Demanda)) +
+  ylab("Demand [kWh]") +
+  xlab("Year") +
+  geom_line() +
+  geom_smooth(method="lm", se=FALSE)
 
 ggAcf(tsShort[,"Demanda"], lag=24)
 
@@ -121,6 +148,7 @@ autoplot(tsShort[,c("PB_Nac","TERMICA","ReservasM3","APORTESM3")], facets=TRUE) 
 qplot(TERMICA, PB_Nac, data=as.data.frame(tsShort)) +
    xlab("Termic ENERGY[kWh]") + ylab("Price of Electricity") #ME GUSTA
 
+
 qplot(ReservasM3, PB_Nac, data=as.data.frame(tsShort)) +
   xlab("Water Reserves[kWh]") + ylab("Price of Electricity") #Casi no me gusta
 
@@ -128,93 +156,185 @@ qplot(APORTESM3, PB_Nac, data=as.data.frame(tsShort)) +
   xlab("Aportes[m3]") + ylab("Price of Electricity") #ME GUSTA
 
 
-GGally::ggpairs(as.data.frame(tsShort[,c("PB_Nac","TERMICA","ReservasM3","APORTESM3","HIDRAULICA")]))
+GGally::ggpairs(as.data.frame(tsShortAlt[,c("PB_Nac","TERMICA","ReservasM3","APORTESM3","HIDRAULICA","Demanda")]))
 
 #AUTOCORRELATION
 ggAcf(tsShort[,"PB_Nac"])
 
+
+###########################################################################
+###########################################################################
+###                                                                     ###
+###                    TIME SERIES REGRESSION MODELS                    ###
+###                                                                     ###
+###########################################################################
+###########################################################################
+
+#Grafica que podemos utilizar para explicar como a falta de energia Hidrica se aumenta el consumo de la termica y aumenta el 
+
+tsShort %>%
+  as.data.frame() %>%
+  ggplot(aes(x=TERMICA, y=PB_Nac)) +
+  ylab("Consumption (quarterly % change)") +
+  xlab("Income (quarterly % change)") +
+  geom_point() +
+  geom_smooth(method="lm", se=FALSE)
+
+tsShort %>%
+  as.data.frame() %>%
+  ggplot(aes(x=HIDRAULICA, y=PB_Nac)) +
+  ylab("Consumption (quarterly % change)") +
+  xlab("Income (quarterly % change)") +
+  geom_point() +
+  geom_smooth(method="lm", se=FALSE)
+
+####################################################################
+#FITTING PB
+
+fit.PB <- tslm(
+  PB_Nac ~ APORTESM3 + HIDRAULICA + TERMICA + Demanda,
+  data=tsShortAlt)
+summary(fit.PB)
+
+#Se puede presentar tsShort(Efecto maximo de el Nino) 2015-2016
+#Se puede presentar tsShortAlt 2018-2019
+
+autoplot(tsShortAlt[,'PB_Nac'], series="Data") +
+  autolayer(fitted(fit.PB), series="Fitted") +
+  xlab("Year") + ylab("") +
+  ggtitle("Price of Electricity") +
+  guides(colour=guide_legend(title=" "))
+
+
+cbind(Data = tsShortAlt[,'PB_Nac'],
+      Fitted = fitted(fit.PB)) %>%
+  as.data.frame() %>%
+  ggplot(aes(x=Data, y=Fitted)) +
+  geom_point() +
+  ylab("Fitted (predicted values)") +
+  xlab("Data (actual values)") +
+  ggtitle("Price of Electricity") +
+  geom_abline(intercept=0, slope=1) #No me gusta
+
+#SELECTING PREDICTORS
+
+##IDEALMENTE SE DEBEN HACER TODAS LAS POSIBLES COMBINACIONES PARA 
+
+CV(fit.PB)
+
+
+###################################################################
+#Evaluating Residuals
+
+checkresiduals(fit.PB)
+
+df <- as.data.frame(tsShortAlt)
+df[,"Residuals"]  <- as.numeric(residuals(fit.PB))
+p1 <- ggplot(df, aes(x=APORTESM3, y=Residuals)) +
+  geom_point()
+p2 <- ggplot(df, aes(x=HIDRAULICA, y=Residuals)) +
+  geom_point()
+p3 <- ggplot(df, aes(x=TERMICA, y=Residuals)) +
+  geom_point()
+p4 <- ggplot(df, aes(x=Demanda, y=Residuals)) +
+  geom_point()
+gridExtra::grid.arrange(p1, p2, p3, p4, nrow=2)
+
+#### Importante hablar de Outliers and influential observations and Spurious Regression
+
+fit.Dem <- tslm(tsSeasonal~ trend + season)
+summary(fit.Dem)
+autoplot(tsSeasonal, series="Data") +
+  autolayer(fitted(fit.Dem), series="Fitted") +
+  xlab("Year") + ylab("Megalitres") +
+  ggtitle("Quarterly Beer Production")
+
+#### WITH FORECASTING
+
+fcast <- forecast(fit.Dem, h=48)
+autoplot(fcast) +
+  ggtitle("Forecasts of energy demand") +
+  xlab("Year") + ylab("kWh")
+
+
+###########################################################################
+###########################################################################
+###                                                                     ###
+###                      TIME SERIES DECOMPOSITION                      ###
+###                                                                     ###
+###########################################################################
+###########################################################################
+
+
+
+#Moving Averages podria servir para mostrar el valor del precio de la electricidad mas claramente
+#Solo dejando la linea roja
+autoplot(tsEE[,"PB_Nac"], series="Data") +
+  autolayer(ma(tsEE[,"PB_Nac"],720), series="24-MA",size=1.2) +
+  xlab("Year") + ylab("price [$/kWh]") +
+  ggtitle("Price of the energy [$]") +
+  scale_colour_manual(values=c("Data"="grey50","24-MA"="red"),
+                      breaks=c("Data","24-MA"))
+
+
+####CON ESTO PODEMOS DEMOSTRAR si es SEASONAL la demanda mientras que el precio no lo es.
+tsSeasonal %>% decompose(type="multiplicative") %>%
+  autoplot() + xlab("Year") +
+  ggtitle("Classical multiplicative decomposition
+    of electrical equipment index")
+
+fit <- stl(tsSeasonal, t.window=48, s.window="periodic",
+           robust=TRUE)
+fit %>% seasadj() %>% naive() %>%
+  autoplot() + ylab("New orders index") +
+  ggtitle("Naive forecasts of seasonally adjusted data")
+
+fit %>% forecast(method="naive") %>%
+  autoplot() + ylab("New orders index")
+
+###########################################################################
+###########################################################################
+###                                                                     ###
+###                                ARIMA                                ###
+###                                                                     ###
+###########################################################################
+###########################################################################
+
+
+#EL PRECIO SE MODELA NO SEASONAL
+fit <- auto.arima(tsEE[,"PB_Nac"], seasonal=FALSE)
+fit
+fit %>% forecast(h=24) %>% autoplot(include=100)
+
+
+ggAcf(tsShort[,"PB_Nac"])
+
+
+fit2 <- Arima(tsShort[,"PB_Nac"], order=c(3,0,0))
+fit2
+fit2 %>% forecast(h=24) %>% autoplot(include=80)
+
+fit3 <- auto.arima(tsShort[,"PB_Nac"], seasonal=FALSE,
+                    stepwise=FALSE, approximation=FALSE)
+fit3
+fit3 %>% forecast(h=24) %>% autoplot(include=80)
+
+
+#LA DEMANDA SE PUEDE MODELAR SEASONAL POR LAS SIGUIENTES HORAS
+
+tsSeasonal %>%
+  Arima(order=c(0,1,1), seasonal=c(0,1,1)) %>%
+  residuals() %>% ggtsdisplay()
+
+
+#TOCA HACER UN RMSE CON LOS POSIBLES VALORES DEL ARIMA Y DEL SEASONAL
+fit3 <- Arima(tsSeasonal, order=c(0,1,3), seasonal=c(0,1,1))
+checkresiduals(fit3)
+
+
+fit3 %>% forecast(h=168) %>% autoplot(include=124)
 #Grafica de demanda y oferta energetica y tsmbien para el agua. 
 
 #Histograma Precio Bolsa Nacional
 
-df_Cor <- data.frame(PB_HIDRAULICA=double(),
-                     PB_TERMICA =double(),
-                     PB_DEM = double(),
-                     PB_ORI = double(),
-                     PB_VALL = double())
-myvars <- c("PB_Nac", "HIDRAULICA","TERMICA","Demanda","ANTIOQUIAMm3","CARIBEMm3","CENTROMm3","ORIENTEMm3","VALLEMm3","Fecha")
-MVC <- dfEE[myvars]
-MVC[is.na(MVC)] <- 0
-MVC <- MVC[108863:157800,]
-for (i in 1:nrow(MVC)) {
-  tempCor <- MVC[i:(i+240),]
-  #print(tempCor)
-  #tempCor[is.na(tempCor)] <- 0
-  CorH20 <- cor(tempCor$PB_Nac, tempCor$HIDRAULICA)
-  CorAN <- cor(tempCor$PB_Nac, tempCor$TERMICA)
-  CorCEN <- cor(tempCor$PB_Nac, tempCor$Demanda)
-  CorORI <- cor(tempCor$PB_Nac, tempCor$ANTIOQUIAMm3)
-  CorVALL <- cor(tempCor$PB_Nac, tempCor$CENTROMm3)
-  
-  
-  df_Cor <- rbind(df_Cor,c(CorH20,CorAN,CorCEN,CorORI,CorVALL))
-  
-}
-
-
-colnames(df_Cor)[1] <- "PB_HIDRAULICA"
-colnames(df_Cor)[2] <- "PB_TERMICA"
-colnames(df_Cor)[3] <- "PB_DEM"
-colnames(df_Cor)[4] <- "PB_ANT"
-colnames(df_Cor)[5] <- "PB_CENT"
-df_Cor <- cbind(df_Cor,MVC$Fecha)
-df_Cor$Fecha <- as.Date(df_Cor$Fecha)
-df_Cor <- cbind(df_Cor,MVC$PB_Nac,MVC$HIDRAULICA,MVC$TERMICA,MVC$Demanda)
-df_Cor$`MVC$PB_Nac`<- df_Cor$`MVC$PB_Nac`/mean(df_Cor$`MVC$PB_Nac`)
-df_Cor$`MVC$HIDRAULICA` <- df_Cor$`MVC$HIDRAULICA`/mean(df_Cor$`MVC$HIDRAULICA`)
-df_Cor$`MVC$TERMICA` <- df_Cor$`MVC$TERMICA`/mean(df_Cor$`MVC$TERMICA`)
-df_Cor$`MVC$Demanda` <- df_Cor$`MVC$Demanda`/mean(df_Cor$`MVC$Demanda`)
-
-colnames(df_Cor)[7] <- "PB_Mean"
-colnames(df_Cor)[8] <- "HIDRA"
-colnames(df_Cor)[9] <- "TERMICA"
-colnames(df_Cor)[10] <- "Demanda"
-df_Cor <- df_Cor[1:8841,]
-
-ggplot(df_Cor, aes(Fecha))+
-  geom_line(aes(y = df_Cor$PB_Mean, colour = "Precio Bolsa"),size=0.8)+
-  geom_line(aes(y = df_Cor$HIDRA ,colour = "Energía Hidráulica"),size=0.5)+
-  geom_line(aes(y = df_Cor$PB_HIDRAULICA ,colour = "Correlación"),size=1.2)+
-  scale_x_date(date_breaks = "1 month", date_labels = "%m %Y") +
-  labs(title = "Correlación Energía Hidráulica y Precio Bolsa", x = "Tiempo [Mes Año]",y ="")
-
-ggplot(df_Cor, aes(Fecha))+
-  geom_line(aes(y = df_Cor$PB_Mean, colour = "Precio Bolsa"))+
-  geom_line(aes(y = df_Cor$TERMICA ,colour = "Energía Termica"))+
-  geom_line(aes(y = df_Cor$PB_TERMICA ,colour = "Correlación"))+
-  scale_x_date(date_breaks = "1 month", date_labels = "%m %Y") +
-  labs(title = "Correlación Energía Termica y Precio Bolsa", x = "Tiempo [Mes Año]",y ="")
-
-ggplot(df_Cor, aes(Fecha))+
-  geom_line(aes(y = df_Cor$PB_Mean, colour = "Precio Bolsa"),size = 0.8)+
-  geom_line(aes(y = df_Cor$Demanda ,colour = "Demanda Total"),size = 0.5)+
-  geom_line(aes(y = df_Cor$PB_DEM ,colour = "Correlación"),size = 1.2)+
-  scale_x_date(date_breaks = "1 month", date_labels = "%m %Y") +
-  labs(title = "Correlación Demanda y Precio Bolsa", x = "Tiempo [Mes Año]",y ="")
-
-ggplot(df_Cor, aes(Fecha))+
-  geom_line(aes(y = df_Cor$PB_Mean, colour = "Precio Bolsa"))+
-  geom_line(aes(y = df_Cor$HIDRA ,colour = "Energía Hidráulica"))+
-  geom_line(aes(y = df_Cor$PB_HIDRAULICA ,colour = "Correlación"))+
-  scale_x_date(date_breaks = "1 month", date_labels = "%m %Y") +
-  labs(title = "Correlación Energía Hidráulica y Precio Bolsa", x = "Tiempo [Mes Año]",y ="")
-
-
-myvars <- c("PB_Nac","VB_Nac","CB_Nac", "HIDRAULICA","TERMICA","Demanda","ANTIOQUIAMm3","CARIBEMm3","CENTROMm3","ORIENTEMm3","VALLEMm3","Date")
-df_cor_cor <- dfEE[70000:120000,]
-df_cor_cor <- df_cor_cor[myvars]
-colnames(df_cor_cor) <- c("Precio Bolsa","Venta Bolsa","Compra Bolsa", "HIDRAULICA","TERMICA","Demanda","ANTIOQUIAMm3","CARIBEMm3","CENTROMm3","ORIENTEMm3","VALLEMm3","Date")
-
-
-ggcorr(df_cor_cor, palette = "RdBu", label = TRUE)
 
